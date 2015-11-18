7d7b1aaa6432f4b140c9f6902bf338684db026ae
Stephanie Kunkel <Rupprich@stud.uni-heidelberg.de>
2015-11-18 14:28:09 +0100

    IMU Viewer works.

--- imu_viewer_osg/imuviewer.cpp
+++ imu_viewer_osg/imuviewer.cpp
@@ -4,10 +4,15 @@
  * http://trac.openscenegraph.org/projects/osg/wiki/Support/Tutorials/BasicGeometry
  * http://trac.openscenegraph.org/projects/osg/attachment/wiki/Support/Tutorials/Tuto2.zip
  * http://trac.openscenegraph.org/projects/osg/attachment/wiki/Support/Tutorials/Tuto9.zip
+ * https://en.wikibooks.org/wiki/Serial_Programming/termios
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h>
 #include <osgViewer/Viewer>
 #include <osg/Node>
 #include <osg/Group>
@@ -23,14 +28,21 @@
 /*
  * Defines
  */
-#define LENGTH 	4.0
-#define WIDTH	4.0
-#define HEIGHT	1.0
+#define LENGTH 		2.0
+#define WIDTH		1.0
+#define HEIGHT		0.1
+//#define DEVICE		"quat.txt"
+#define DEVICE		"/dev/ttyACM0"
+#define BAUDRATE 	B115200
 
 /*
  * Function Prototypes
  */
-void updateScene(FILE* fp);
+void updateScene(int tty_fd);
+int configureUart(int tty_fd);
+int readUartLine(int tty_fd, char* line);
+int configureStdout();
+int printToStdout(char* line);
 osg::ref_ptr<osg::Group> createLight();
 osg::Geode* createIMU(float length, float width, float height);
 
@@ -45,8 +57,12 @@ osg::PositionAttitudeTransform* transform;
 int main(){
 	//Variables
 	osgViewer::Viewer viewer;
+	osg::PositionAttitudeTransform* rootnode = new osg::PositionAttitudeTransform;
 	transform = new osg::PositionAttitudeTransform;
-	FILE* fp;
+	int tty_fd;
+	int status;
+	char* line = (char* ) malloc(500);
+	char c;
 	
 	//Light
 	osg::ref_ptr<osg::Group> lightGroup = createLight();
@@ -58,14 +74,53 @@ int main(){
 	//Transform
 	transform->setPosition(osg::Vec3(0.0f, 0.0f, 0.0f));
 	transform->addChild(imuGeode);
-	viewer.setSceneData(transform); 
+	
+	//Root
+	rootnode->setPosition(osg::Vec3(0.0f, -5.0f, 1.0f));
+	//rootnode->setAttitude(osg::Quat(0.5, 0.5, 0.5, 0.5)); view IMU from above
+	rootnode->setAttitude(osg::Quat(0.35355339059327384, 0.6123724356957946, 0.35355339059327384, 0.6123724356957945));
+    rootnode->addChild(transform);
+	
+	//Viewer
+	viewer.setSceneData(rootnode); 
 	viewer.setCameraManipulator(new osgGA::TrackballManipulator());
+	
+	//Configure STDOUT
+	/*status = configureStdout();
+	if (status != 0){
+		strcpy(line, "Could not configure STDOUT.\n\r");
+		printToStdout(line);
+		return 0;
+	}
+	strcpy(line, "STDOUT Configuration done.\n\r");
+	printToStdout(line);
+	*/
+	
+	
+	//Open UART Connection
+	strcpy(line, "chmod +r ");
+	strcat(line, DEVICE);
+	status = system(line);
+	tty_fd = open(DEVICE,  O_RDWR | O_NOCTTY | O_NDELAY);
+	if (tty_fd == -1){
+		printf("Could not open port\r\n");
+		return 0;
+	}
+	
+	//Configure UART
+	status = configureUart(tty_fd);
+	
+	
 	
 	//Run
 	while(!viewer.done()){
 		viewer.frame();
-		updateScene(fp);
+		updateScene(tty_fd);
+		sleep(1);//usleep(16666);
 	}
+	
+	//Close UART Connection
+	close(tty_fd);
 	
 	//Return
 	return 0;
@@ -74,42 +129,129 @@ int main(){
 /*
  * Update setScene
  */
-void updateScene(FILE* fp){
+void updateScene(int tty_fd){
 	//Variables
+	osg::Quat quat;
 	float w, x, y, z;
 	int status;
+	char* line = (char*) malloc(500);
 	
-	//Open UART Connection
-	status = system("chmod +x /dev/ttyACM0");
+	//Initialize Quaternion
+	quat = transform->getAttitude();
+	w = quat.w();
+	x = quat.x();
+	y = quat.y();
+	z = quat.z();
+	
+	//Get Line from UART
+	status = readUartLine(tty_fd, line);
 	if (status != 0){
-		printf("Could not set permissions.\r\n");
+		//printf("Could not read from UART (%d).\r\n", status);
 		return;
 	}
-	fp = fopen("/dev/ttyACM0", "r");
 	
-	//Get Quaternion from UART
-	if (fp == NULL){
-		printf("Could not open file\r\n");
+	//printf("%s\n\r", line);
+	
+	//Get Quaternions
+	status = sscanf(line, "%f %f %f %f", &w, &x, &y, &z);
+	if (status != 4 || w > 1.0 || x > 1.0 || y > 1.0 || z > 1.0){
+		printf("Could not get Quaternion (%d).\r\n", status);
 		return;
-	} else {
-		status = fscanf(fp, "%f %f %f %f", &w, &x, &y, &z);
-		if (status != 4){
-			printf("Could not get Quaternions.\r\n");
-			return;
-		}
-		
-		printf(Quat: %f %f %f %f\r\n", %w, %x, %y, %z);
-
-		//Close UART Connection
-		status = fclose(fp);
-		if (status != 0){
-			printf("Could not close file.\r\n");
-		}
-		
-		//Update Scene
-		osg::Quat quat = osg::Quat(w, x, y, z);
-		transform->setAttitude(osg::Quat(w, x, y, z));
 	}
+	
+	printf("%f %f %f %f\n\r", w, x, y, z);
+	
+	//Update Scene
+	quat = osg::Quat(w, x, y, z);
+	transform->setAttitude(osg::Quat(w, x, y, z));
+}
+
+
+/*
+ * Configure UART Connection
+ */
+int configureUart(int tty_fd){
+	//Variables
+	struct termios tio;
+	
+	//Configuration
+	memset(&tio,0,sizeof(tio));
+	tio.c_iflag=0;
+	tio.c_oflag=0;
+	tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+	tio.c_lflag=0;
+	tio.c_cc[VMIN]=1;
+	tio.c_cc[VTIME]=5;
+
+	//Open device file
+	tty_fd=open(DEVICE, O_RDWR | O_NONBLOCK);      
+	cfsetospeed(&tio,BAUDRATE);            // 115200 baud
+	cfsetispeed(&tio,BAUDRATE);            // 115200 baud
+	
+	//Configure
+	tcsetattr(tty_fd,TCSANOW,&tio);
+	
+	//if (read(tty_fd,&c,1)>0)        write(STDOUT_FILENO,&c,1);              // if new data is available on the serial port, print it out
+	return 0;
+}
+
+/*
+ * Read Line from UART
+ */
+int readUartLine(int tty_fd, char* line){
+	char c;
+	strcpy(line, "");
+
+	while(read(tty_fd,&c,1)>0){
+		if (c != '\n'){
+			strcat(line, &c);
+		} else {
+			return 0;
+		}
+	}
+	
+	return -1;
+
+}
+
+/*
+ * Open STDOUT
+ */
+int configureStdout(){
+	//Variables
+	struct termios stdio;
+	
+	//Configuration
+	memset(&stdio,0,sizeof(stdio));
+	stdio.c_iflag=0;
+	stdio.c_oflag=0;
+	stdio.c_cflag=0;
+	stdio.c_lflag=0;
+	stdio.c_cc[VMIN]=1;
+	stdio.c_cc[VTIME]=0;
+	
+	//Configure
+	tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
+	tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
+	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
+	
+	return 0;
+}
+
+/*
+ * Print line to STDOUT
+ */
+int printToStdout(char* line){
+	//Variables
+	int length, status;
+	
+	//Get length
+	length = strlen(line);
+	
+	//Write
+	status = write(STDOUT_FILENO, &line, length);
+	
+	return status;
 }
 
 /*
@@ -158,14 +300,14 @@ osg::ref_ptr<osg::Group> createLight(){
  */
 osg::Geode* createIMU(float length, float width, float height) {
 	//Coordinates
-	osg::Vec3 A = osg::Vec3(-0.5f*length, -0.5f*width, 0.5f*height);
-	osg::Vec3 B = osg::Vec3(0.5f*length, -0.5f*width, 0.5f*height);
-	osg::Vec3 C = osg::Vec3(0.5f*length, 0.5f*width, 0.5f*height);
-	osg::Vec3 D = osg::Vec3(-0.5f*length, 0.5f*width, 0.5f*height);
-	osg::Vec3 E = osg::Vec3(-0.5f*length, -0.5f*width, -0.5f*height);
-	osg::Vec3 F = osg::Vec3(0.5f*length, -0.5f*width, -0.5f*height);
-	osg::Vec3 G = osg::Vec3(0.5f*length, 0.5f*width, -0.5f*height);
-	osg::Vec3 H = osg::Vec3(-0.5f*length, 0.5f*width, -0.5f*height);
+	osg::Vec3 A = osg::Vec3(-0.5f*height, -0.5f*length, 0.5f*width);
+	osg::Vec3 B = osg::Vec3(0.5f*height, -0.5f*length, 0.5f*width);
+	osg::Vec3 C = osg::Vec3(0.5f*height, 0.5f*length, 0.5f*width);
+	osg::Vec3 D = osg::Vec3(-0.5f*height, 0.5f*length, 0.5f*width);
+	osg::Vec3 E = osg::Vec3(-0.5f*height, -0.5f*length, -0.5f*width);
+	osg::Vec3 F = osg::Vec3(0.5f*height, -0.5f*length, -0.5f*width);
+	osg::Vec3 G = osg::Vec3(0.5f*height, 0.5f*length, -0.5f*width);
+	osg::Vec3 H = osg::Vec3(-0.5f*height, 0.5f*length, -0.5f*width);
 	
 	//Shape 
 	osg::Vec3Array* coords = new osg::Vec3Array;
