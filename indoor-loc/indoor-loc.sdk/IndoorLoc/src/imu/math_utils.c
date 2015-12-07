22753f875f6997331d0dff325812de01b98182ac
Stephanie Kunkel <Rupprich@stud.uni-heidelberg.de>
2015-12-07 16:49:53 +0100

    Added methods for vector and matrix operations as well as for converting quat to rotation matrix and discrete integration.

--- indoor-loc/indoor-loc.sdk/IndoorLoc/src/imu/math_utils.c
+++ indoor-loc/indoor-loc.sdk/IndoorLoc/src/imu/math_utils.c
@@ -6,6 +6,35 @@
 #include "math_utils.h"
 
 /*
+ * Discrete Integration: result = recent + scalar * vector
+ * In: scalar, vector, recent
+ * Out: result
+ */
+void discreteIntegration(float scalar, Vector* vector, Vector* recent, Vector* result){
+	*result = addVectors(*recent, multVectorByScalar(*vector, scalar));
+}
+
+/*
+ * Get Roation Matrix from Quaternion
+ * In: Quaternion
+ * Out: Rotation Matrix
+ */
+void toRotationMatrix(float quat[QUATERNION_AMOUNT], Matrix* rotationMatrix) {
+	rotationMatrix->value[0] = quat[0] * quat[0] + quat[1] * quat[1]
+			- quat[2] * quat[2] - quat[3] * quat[3];
+	rotationMatrix->value[1] = 2 * quat[1] * quat[2] - 2 * quat[0] * quat[3];
+	rotationMatrix->value[2] = 2 * quat[1] * quat[3] + 2 * quat[0] * quat[2];
+	rotationMatrix->value[3] = 2 * quat[1] * quat[2] + 2 * quat[0] * quat[3];
+	rotationMatrix->value[4] = quat[0] * quat[0] - quat[1] * quat[1]
+			+ quat[2] * quat[2] - quat[3] * quat[3];
+	rotationMatrix->value[5] = 2 * quat[2] * quat[3] + 2 * quat[0] * quat[1];
+	rotationMatrix->value[6] = 2 * quat[1] * quat[3] - 2 * quat[0] * quat[2];
+	rotationMatrix->value[7] = 2 * quat[2] * quat[3] - 2 * quat[0] * quat[1];
+	rotationMatrix->value[8] = quat[0] * quat[0] - quat[1] * quat[1]
+			- quat[2] * quat[2] + quat[3] * quat[3];
+}
+
+/*
  * Euler Angles
  */
 void eulerGetSigma(float quat[QUATERNION_AMOUNT], float* sigma) {
@@ -34,4 +63,103 @@ long floatToQ16(float x) {
  */
 float q16ToFloat(long q16) {
 	return (float) q16 / (1L << 16);
+}
+
+/*
+ * Multiply Vector by Scalar
+ * In: Vector, Scalar
+ * Returns Vector
+ */
+Vector multVectorByScalar(Vector vector, float scalar) {
+	//Variables
+	int i;
+	Vector result;
+
+	//Multiply each element by scalar
+	for (i = 0; i < NUMBER_OF_AXES; i++) {
+		result.value[i] = vector.value[i] * scalar;
+	}
+
+	//Return
+	return result;
+}
+
+/*
+ * Vector Addition
+ * In: two Vectors
+ * Returns Vector
+ */
+Vector addVectors(Vector vector1, Vector vector2) {
+	//Variables
+	int i;
+	Vector result;
+
+	//Add each element
+	for (i = 0; i < NUMBER_OF_AXES; i++) {
+		result.value[i] = vector1.value[i] + vector2.value[i];
+	}
+
+	//Return
+	return result;
+}
+
+/*
+ * Vector Subtraction
+ * In: two Vectors
+ * Returns Vector result
+ */
+Vector substractVectors(Vector minuend, Vector substrahend) {
+	//Variables
+	int i;
+	Vector result;
+
+	//Add each element
+	for (i = 0; i < NUMBER_OF_AXES; i++) {
+		result.value[i] = minuend.value[i] - substrahend.value[i];
+	}
+
+	//Return
+	return result;
+}
+
+/*
+ * Multiplication: Scalar * Matrix
+ * In: Matrix, Scalar
+ * Returns Matrix
+ */
+Matrix multMatrixByScalar(Matrix matrix, float scalar) {
+	//Variables
+	int i;
+	Matrix result;
+
+	//Multiply each element by scalar
+	for (i = 0; i < NUMBER_OF_AXES * NUMBER_OF_AXES; i++) {
+		result.value[i] = matrix.value[i] * scalar;
+	}
+
+	//Return
+	return result;
+}
+
+/*
+ * Multiplication: Matrix * Vector
+ * In: Matrix, Vector
+ * Returns Vector
+ */
+Vector multMatrixAndVector(Matrix matrix, Vector vector) {
+	//Variables
+	int i, j;
+	Vector result;
+
+	//Multiply each element by scalar
+	for (i = 0; i < NUMBER_OF_AXES; i += NUMBER_OF_AXES) { //row index
+		result.value[i] = 0.0;
+		for (j = 0; j < NUMBER_OF_AXES; j++) { //column index
+			result.value[i] += matrix.value[i * NUMBER_OF_AXES + j]
+					* vector.value[i];
+		}
+	}
+
+	//Return
+	return result;
 }
