d740868a240d591e338264f0a9f9c971c160cfb2
Stephanie Kunkel <Rupprich@stud.uni-heidelberg.de>
2016-03-06 09:06:24 +0100

    Further work on Kalman filter.

--- output/Calibration2502/calibratedColdStart/quatVelPos_cal_fil_kalman.m
+++ output/Calibration2502/calibratedColdStart/quatVelPos_cal_fil_kalman.m
@@ -28,9 +28,9 @@ function angle_out = kalman_angle(angle_in, rate, stddev_angle, stddev_rate, del
   var_rate = sqrt(stddev_rate);
 
   # Initialize
-  y = [angle_in(1); rate(1)];
+  y_hat = [angle_in(1); rate(1)];
   angle_out(1) = angle_in(1);
-  A = [1 -delta_t; 0 1];
+  A = [1 (delta_t); 0 1];
   B = [delta_t; 0];
   H = [1 0];
   P = [0 0; 0 0];
@@ -39,19 +39,22 @@ function angle_out = kalman_angle(angle_in, rate, stddev_angle, stddev_rate, del
   R = [stddev_angle 0; 0 stddev_rate];
 
   # Go through samples
-  for i=2:size(angle_in)
+  for i=2:length(angle_in)
   
     # Rememver previous error covariance
     P_prev = P;
 
     # Prediction Update
-    angle_est = A*angle_out(i-1) + B*rate(i);
+    y_hat_neg = A*y_hat + B*rate(i);
     P = A*P_prev*transpose(A) + Q;
 
     # Measurement Update
-    K = P_prev*transpose(H)*inv(H*P_prev*transpose(H));
-    angle_out(i) = angle_est + K*(angle - H*angle_est);
+    K = P_prev*transpose(H)*inverse(H*P_prev*transpose(H) + R);
+    y_hat = y_hat_neg + K*([angle_in(i); rate(i)] - H*y_hat_neg);
     P = (eye(2,2) - K*H)*P_prev;
+    
+    # Store values
+    angle_out(i) = y_hat(1,1);
   end
 
 endfunction
@@ -105,7 +108,7 @@ for i = 1:length(gx)
   # Normalize Acc
   acc_mag = sqrt(ax(i)^2 + ay(i)^2 + az(i)^2);
   if (acc_mag != 0)
-	acc_norm = [(ax(i) / acc_norm_sc); (ay(i) / acc_norm_sc); (az(i) / acc_norm_sc)];
+		acc_norm = [(ax(i) / acc_mag); (ay(i) / acc_mag); (az(i) / acc_mag)];
   else 
     acc_norm = [ax(i); ay(i); az(i)];
   endif;
@@ -125,7 +128,7 @@ for i = 1:length(gx)
     angle_gyr_y(i) = angle_gyr_y(i-1) + gx(i) * delta_t;
     angle_gyr_z(i) = angle_gyr_z(i-1) + gx(i) * delta_t;
   endif;
-  
+
 endfor
 
 # Kalman Filter for x axis
@@ -133,9 +136,60 @@ angle_x = kalman_angle(angle_acc_x, gx, stddev_angle_x, stddev_rate_x, delta_t);
 angle_y = kalman_angle(angle_acc_y, gy, stddev_angle_y, stddev_rate_y, delta_t);
 angle_z = kalman_angle(angle_acc_z, gz, stddev_angle_z, stddev_rate_z, delta_t);
 
-# Plot
+# Plot x
+plot(angle_gyr_x, "r");
+hold on;
+plot(angle_acc_x, "g");
+hold on;
+plot(angle_x, "c");
+hold on;
 
+# Set up Plot
+grid on;
+title('Sensor Fusion using Kalman Filter');
+xlabel('Sample Number');
+ylabel('Rotation around x-axis (dgr)');
+legend('Gyro only', 'Accel only', 'Kalman');
 
-# clear
+# Print
+print(outfile);
+hold off;
+
+# Plot y
+plot(angle_gyr_y, "r");
+hold on;
+plot(angle_acc_y, "g");
+hold on;
+plot(angle_y, "c");
+hold on;
+
+# Set up Plot
+grid on;
+title('Sensor Fusion using Kalman Filter');
+xlabel('Sample Number');
+ylabel('Rotation around y-axis (dgr)');
+legend('Gyro only', 'Accel only', 'Kalman');
+
+# Print
+print("-append", outfile);
+hold off;
+
+# Plot z
+plot(angle_gyr_z, "r");
+hold on;
+plot(angle_acc_z, "g");
+hold on;
+plot(angle_z, "c");
+hold on;
+
+# Set up Plot
+grid on;
+title('Sensor Fusion using Kalman Filter');
+xlabel('Sample Number');
+ylabel('Rotation around y-axis (dgr)');
+legend('Gyro only', 'Accel only', 'Kalman');
+
+# Print
+print("-append", outfile);
 hold off;
 
